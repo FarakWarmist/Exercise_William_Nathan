bf4cd11423d3f44232607a08109891341ce6c1e4
William Sylvestre <farakwarts@gmail.com>
2023-09-26 10:46:05 -0400

    Ajout choix de programme

--- Exercice_William_Nathan/Exercice_William_Nathan/Program.cs
+++ Exercice_William_Nathan/Exercice_William_Nathan/Program.cs
@@ -4,7 +4,43 @@
     {
         static void Main(string[] args)
         {
-            new Metric();
+            String? choix;
+
+            Console.WriteLine("\t\tBienvenue à l'exercice de William & Nathan !");
+
+            Console.WriteLine("Quelle option voulez vous tester? :");
+            Console.WriteLine("Quizz [Q] | Monnaie [Mo] | Metric [Me]");
+            Console.WriteLine("Ou tapez «EXIT» pour quitter le programme.");
+
+            while (true)
+            {
+                choix = Console.ReadLine();
+
+                if (choix == "Q" || choix == "q")
+                {
+                    new Quizz();
+                    break;
+                }
+                else if (choix == "Mo" || choix == "mo")
+                {
+                    new Monnaie();
+                    break;
+                }
+                else if (choix == "Me" || choix == "me")
+                {
+                    new Metric();
+                    break;
+                }
+                else if (choix == "EXIT" || choix == "exit")
+                {
+                    System.Environment.Exit(0);
+                }
+                else
+                {
+                    Console.WriteLine("Veuillez choisir parmi une des option suivante :");
+                    Console.WriteLine("Quizz [Q] | Monnaie [Mo] | Metric [Me]");
+                }
+            }
         }
     }
 }

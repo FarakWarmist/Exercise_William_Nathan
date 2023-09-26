b3b9d34e63e493c8ccc6c9a0ef7b0b270c9c27a7
William Sylvestre <farakwarts@gmail.com>
2023-09-26 09:46:35 -0400

    Ajout document Visual Studio

---
+++ Exercice_William_Nathan/Exercice_William_Nathan/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Exercice_William_Nathan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
